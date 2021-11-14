8e4ce0d9f6a9396e849320be9b2dec7915754cec
Yosr Abdelhamid <yosr.abdelhamid97@gmail.com>
2021-11-14 14:26:23 +0100

    "Use this"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { DepartementsComponent } from './departements/departements.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MedecinsComponent } from './medecins/medecins.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'departements', component: DepartementsComponent },
+  { path: 'medecins', component: MedecinsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,32 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { DepartementsComponent } from './departements/departements.component';
+import { MedecinsComponent } from './medecins/medecins.component';
+import { ContactComponent } from './contact/contact.component';
+import { ActualitesComponent } from './actualites/actualites.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    AboutComponent,
+    ServicesComponent,
+    DepartementsComponent,
+    MedecinsComponent,
+    ContactComponent,
+    ActualitesComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
